30c52fa5f9df1ea9a474a6563100723c465fb496
Natasyasya <raranatasyafitriani@gmail.com>
2024-09-18 08:29:06 +0700

    Add files via upload
    menambah file baru

---
+++ 116-Nafhatuz/footer.php
@@ -0,0 +1,8 @@
+<footer>
+<p>&copy; <?php echo date('Y'); ?> Universitas Trunojoyo Madura. All rights reserved.</p>
+<p>Alamat: Jl. Raya Telang Indah , Kamal, Bangkalan</p>
+<p>Email: 220631100113@student.trunojoyo.co.id| Telepon: 083 162130987</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
