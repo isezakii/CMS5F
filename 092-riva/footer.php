7dbf4998bb27eb634e08ac9a389ea145c7133a19
raykamajaya <133558308+raykamajaya@users.noreply.github.com>
2024-09-18 08:21:58 +0700

    Up By Reka

---
+++ 092-riva/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <p>&copy; <?php echo date('Y'); ?> Universitas Trunojoyo Madura. All rights reserved.</p>
+    <p>Alamat: Jl. Raya PO Box 2 Kamal Bangkalan Madura. Indonesia</p>
+    <p>Email: info@tronojoyo.ac.id | Telepon: (031) 123-4567</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+
+</html>
