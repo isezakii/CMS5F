e772710127347e035194a5167907d03b963257f7
Yuda2404 <yudaaprinorhadi04@gmail.com>
2024-09-18 08:28:21 +0700

    upload file themes

---
+++ 107-Yudaapri/footer.php
@@ -0,0 +1,8 @@
+<footer>
+<p>&copy; <?php echo date('Y'); ?> Nama Institusi. All rights reserved.</p>
+<p>Alamat: Jl. Contoh No. 123, Kota Contoh, Negara</p>
+<p>Email: info@institusi.com | Telepon: (021) 123-4567</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
